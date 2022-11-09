df3c7c1b9e80b0d5e263a9d6ef9fa60fa95733ca
“we-die-tommorow” <jojongumbi8@gmail.com>
2022-11-10 00:17:21 +0300

    new HFT

---
+++ index.js
@@ -0,0 +1,59 @@
+
+ const MoodContractAddress = "0x394814f3225f2E89B0Eb5C20af3b7C61bdaa3573";
+  const MoodContractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_mood",
+				"type": "string"
+			}
+		],
+		"name": "setMood",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getMood",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+  let MoodContract;
+  let signer;
+
+
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+
+  provider.send("eth_requestAccounts", []).then(() => {
+  provider.listAccounts().then((accounts) => {
+    signer = provider.getSigner(accounts[0]);
+    MoodContract = new ethers.Contract(
+      MoodContractAddress,
+      MoodContractABI,
+      signer
+    );
+  });
+});
+
+async function getMood() {
+  const getMoodPromise = MoodContract.getMood();
+  const Mood = await getMoodPromise;
+  console.log(Mood);
+}
+
+async function setMood() {
+  const mood = document.getElementById("mood").value;
+  const setMoodPromise = MoodContract.setMood(mood);
+  await setMoodPromise;
+}
